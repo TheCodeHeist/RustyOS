19a1f3674508a321d774efad344f7eebea2ed325
Md. Riyasat Hossain <riyasat.hossain.29@gmail.com>
2022-12-26 17:17:04 +0600

    IT WORKS!!

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+static HELLO: &[u8] = b"Welcome to RustyOS!!!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -17,7 +17,7 @@ pub extern "C" fn _start() -> ! {
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xa;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xc;
         }
     }
 
