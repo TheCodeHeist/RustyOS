2a7e507f114605a1690b9af5600add81d887127c
Md. Riyasat Hossain <riyasat.hossain.29@gmail.com>
2022-12-28 13:00:58 +0600

    Double Fault implemented...

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ pub extern "C" fn _start() -> ! {
 
     rusty_os::init();
 
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    }
+
     // invoke a breakpoint exception
     x86_64::instructions::interrupts::int3();
 
@@ -20,6 +24,7 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     println!("It did not crash!");
+
     #[allow(clippy::empty_loop)]
     loop {}
 }
